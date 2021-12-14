1bdf97e3d6a4c8083c4360341c77d225a98db569
Azimul Islam <86977364+azimulislma266@users.noreply.github.com>
2021-12-14 08:07:32 +0600

    Add files via upload

---
+++ Array & Basic Methods --12/app.js
@@ -0,0 +1,119 @@
+//Array hocce conction of veluo;
+
+// arry hocce onek sonkha ka akta veribel ar betor atkate pari
+
+
+let user = ["Azimul", " kapr", " Kuddos"," jobbar", " akkas"," mossiur"]
+
+
+
+console.log ( user[3]);
+
+
+// let user = [];
+
+// user [0] = " Azimul islam";
+
+// user [1] = " Sumon islam Shanto ";
+
+// user [2] = " Talebali";
+
+// user [3] = " Sabik";
+
+// user [4] = " hakem ali";
+
+// user [5] = " tota";
+
+
+// console.log ( user[ 4]);
+
+
+// let user = new Array ();
+
+// user [0] = " Azimul islam";
+
+// user [1] = " Sumon islam Shanto ";
+
+// user [2] = " Talebali";
+
+// user [3] = " Sabik";
+
+// user [4] = " hakem ali";
+
+// user [5] = " tota";
+
+
+// console.log ( user)
+
+
+
+// normol Array
+
+
+// let user =[
+
+// " Nasim",
+// " Kuddos",
+// " Kamal kaka",
+// " Pagla",
+// " Monacif",
+// " Rustum",
+// "Nepu",
+
+// ]
+
+// user.pop(); last akta velow jode delete kore deya
+// user.push("pagla"); last new akta velou add kore deva
+// user.unshift("Shovo"); age akta velou add kora
+// user.shift();frist akta velou keta deva ,
+
+// console .log(user);
+// /usersole.log ( user[0])re console ar a [] ta
+
+// delete user [1]; sob gula keta deya se akta dekhabe 
+// user.length 
+
+
+
+
+// for lup ta bsi us kora hoy
+
+
+// for (let index = 0; index < user.length; index++) { 
+
+//     console.log ( user[index]);
+    
+// }
+
+
+
+// user.length // ar maning hocce 5;
+// console.log ( user[user.length -1]); amra last array ta access korta chi tahole amra length number deya lst ta assice korta parbo
+
+
+// abebo show korano jay arry gulo 
+
+// for (user of user) {
+   
+//     console.log ( user)
+    
+// }
+
+
+let user =[
+ 
+    [ 'Esa', ' tota ', ' jobbar', ' khobis', ' mosa', ' bandor', ' khatal', ' koduajd'],
+    [ 'Pori', ' tota ', ' jobbar', ' khobis', ' mosa', ' bandor', ' khatal', ' koduajd'],
+    [ 'Esa', ' tota ', ' jobbar', ' khobis', ' mosa', ' bandor', ' khatal', ' koduajd'],
+    [ 'Esa', ' tota ', ' jobbar', ' khobis',  
+
+    [ 'kuddos', ' tota ', ' jobbar', ' khobis', ' mosa', ' bandor', 'khatal', 'koduajd'],
+
+    ' mosa', ' bandor', ' khatal', ' koduajd'],
+
+
+]
+
+// console.log ( user [0][0]); Exampol
+
+ console.log ( user [2] [2] [4]);
